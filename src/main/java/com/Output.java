d137d12796697e6096e71c5c7b9b883bce4bf338
Taj Bist <tajbist2@gmail.com>
2016-11-21 13:18:40 -0500

    New file

--- src/main/java/com/Output.java
+++ src/main/java/com/Output.java
@@ -3,9 +3,8 @@ package com;
 /**
  * Created by Oops on 11/21/2016.
  */
-public class Output
-{
-    public static void main(String arg[]){
-        System.out.println("Hello World.");
+public class Output {
+    public static void main(String rg[]) {
+        System.out.println("Hello WOrld");
     }
 }
